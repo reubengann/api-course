8cfe3f108fcefa526ad1387863bc3c2cbc0c1cf1
Reuben Gann <reubengann@gmail.com>
2022-01-09 16:48:57 -0500

    Add create post endpoint

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from typing import Any, Dict
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello, world'}
+
+@app.get("/")
+async def root() -> dict:
+    return {"message": "Hello, world"}
+
+
+@app.get("/posts")
+async def get_posts() -> dict:
+    return {"data": "Here's the post"}
+
+
+@app.post("/createpost")
+async def create_post(payload: Dict[str, Any] = Body(...)) -> dict:
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
