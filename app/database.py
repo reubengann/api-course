37cb595a503e5696e5bad25b2cf7b09235dfb223
Reuben Gann <reubengann@gmail.com>
2022-01-23 23:13:34 -0500

    Set database url in environment

--- app/database.py
+++ app/database.py
@@ -5,35 +5,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL is None:
+    raise Exception("DATABASE_URL is undefined")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-def ensure_created():
-    if not os.path.exists("test.db"):
-        conn = sqlite3.connect("test.db")
-        cursor = conn.cursor()
-        cursor.execute(
-            """create table posts (
-    id integer primary key autoincrement,
-    title varchar(255) not null,
-    content text not null,
-    published bool not null,
-    rating int null
-    )"""
-        )
-        conn.commit()
-    else:
-        conn = sqlite3.connect("test.db")
-        cursor = conn.cursor()
-
-    cursor.row_factory = sqlite3.Row
-    return conn, cursor
 
 
 def get_db():
