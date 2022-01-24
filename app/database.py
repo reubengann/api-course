c0b0af079df96d39733351dc47b61ab11c2eb86d
Reuben Gann <reubengann@gmail.com>
2022-01-23 23:37:10 -0500

    Use pydantic to get environment variables

--- app/database.py
+++ app/database.py
@@ -1,15 +1,12 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from . import settings
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL is None:
-    raise Exception("DATABASE_URL is undefined")
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    settings.settings.database_url, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,22 +1,22 @@
 import datetime
-import os
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-from . import schemas
+from . import schemas, settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-if SECRET_KEY is None:
-    raise Exception("SECRET_KEY must be set as an environment variable")
+SECRET_KEY = settings.settings.secret_key
 ALGORITHM = jwt.ALGORITHMS.HS256
 ACCESS_TOKEN_EXPIRATION_MINUTES = 30
 
 
-class BadToken(Exception):
-    pass
+BadToken = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail=f"Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
 
 
 def create_access_token(payload: dict) -> str:
