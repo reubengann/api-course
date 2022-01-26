df40af56f38f3067ef9d12c82d260a0b139ff677
Reuben Gann <reubengann@gmail.com>
2022-01-26 00:11:30 -0500

    Remove auto-generate db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 
-from .database import engine
-from . import orm
 from .routers import user, post, auth, vote
 
 app = FastAPI()
@@ -9,5 +7,3 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-
-orm.Base.metadata.create_all(engine)
