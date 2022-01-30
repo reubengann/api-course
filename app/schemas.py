117c3055a01b5af3241ef0a82b73af96284969d8
Reuben Gann <reubengann@gmail.com>
2022-01-30 00:22:44 -0500

    Add login test

--- app/schemas.py
+++ app/schemas.py
@@ -14,6 +14,7 @@ class PostCreate(Post):
 
 
 class UserCreateResponse(BaseModel):
+    id: int
     email: EmailStr
 
     class Config:
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,30 @@
-from .db import session, client
+import pytest
+from jose import jwt
+from app.settings import settings
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "test@example.com", "password": "password123"}
+    result = client.post("/users/", json=user_data).json()
+    result["password"] = user_data["password"]
+    return result
 
 
 def test_create_user(client):
     result = client.post(
-        "/users/", json={"email": "test@example.com", "password": "password123"}
+        "/users/", json={"email": "test2@example.com", "password": "password123"}
     )
     assert result.status_code == 201
-    assert result.json().get("email") == "test@example.com"
+    assert result.json().get("email") == "test2@example.com"
+
+
+def test_login_user(client, test_user):
+    result = client.post(
+        "/login/", json={"email": test_user["email"], "password": test_user["password"]}
+    )
+    token = result.json().get("access_token")
+    token = jwt.decode(token, settings.secret_key, ["HS256"])
+    assert token.get("id") == test_user["id"]
+    assert result.json().get("token_type") == "bearer"
+    assert result.status_code == 200
