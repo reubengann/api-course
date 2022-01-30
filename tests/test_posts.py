ed7f161898cd01046c044635d89c98366956307c
Reuben Gann <reubengann@gmail.com>
2022-01-30 01:45:17 -0500

    Add test_unauthorized_user_create

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -25,3 +25,10 @@ def test_create_post(authorized_client, test_user, example_posts):
     response = res.json()
     assert response["title"] == "Cool ass title"
     assert response["published"]
+
+
+def test_unauthorized_user_create(client):
+    res = client.post(
+        "/posts/", json={"title": "Cool ass title", "content": "ooby dooby"}
+    )
+    assert res.status_code == 401
