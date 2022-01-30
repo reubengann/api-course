fc88260322d8a7aa86510af024cd830b4c0cc50f
Reuben Gann <reubengann@gmail.com>
2022-01-30 01:20:06 -0500

    Add get specific post test

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -2,3 +2,10 @@ def test_get_all_posts(authorized_client, example_posts):
     response = authorized_client.get("/posts/")
     assert response.status_code == 200
     assert len(example_posts) == len(response.json())
+
+
+def test_get_one_posts(authorized_client, example_posts):
+    post_id = example_posts[0].id
+    response = authorized_client.get(f"/posts/{post_id}")
+    assert response.status_code == 200
+    assert response.json()["content"] == example_posts[0].content
